83da6697c19a6735b0efe9321411cb80ffc651ce
mistergraphx <arnaud.berard@mister-graphx.com>
2026-04-22 15:10:22 +0200

    disable nvm

--- main.zsh
+++ main.zsh
@@ -21,7 +21,7 @@ fi
 #   ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/oh-my-zsh"
 # fi
 # echo $ZSH_CACHE_DIR
-# Create cache and completions dir and add to $fpath
+# Create cache dir and add to $fpath
 mkdir -p "$ZSH_CACHE_DIR"
 (( ${fpath[(Ie)$ZSH_CACHE_DIR]} )) || fpath=("$ZSH_CACHE_DIR" $fpath)
 
@@ -89,10 +89,10 @@ source "${ZSH}/themes/agnoster-zsh-theme/agnoster.zsh-theme"
 source <(fzf --zsh)
 source "${ZSH}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "${ZSH}/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
-export NVM_AUTO_USE=false    # change or install node version using .nvmrc files from projects paths
-export NVM_COMPLETION=true
-export NVM_LAZY_LOAD=true
-source "${ZSH}/plugins/zsh-nvm/zsh-nvm.plugin.zsh"
+# export NVM_AUTO_USE=false    # change or install node version using .nvmrc files from projects paths
+# export NVM_COMPLETION=true
+# export NVM_LAZY_LOAD=true
+# source "${ZSH}/plugins/zsh-nvm/zsh-nvm.plugin.zsh"
 
 # Autoload user functions
 if [[ -d "${ZSH}/functions" ]]; then
@@ -109,8 +109,9 @@ fpath=(
 )
 
 # Completion
+# https://zsh.sourceforge.io/Doc/Release/Completion-System.html
 # ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#b4b4b9"
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_STRATEGY=(completion history)
 ZSH_COMPDUMP="${ZSH_CACHE_DIR}/.zcompdump"
 
 setopt menu_complete  # Automatically highlight first element of completion menu
@@ -125,14 +126,12 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 # git completion
 zstyle ':completion:*:*:git:*' script $ZSH/plugins/git-completions/git-completion.bash
 
-autoload -Uz compinit zrecompile
+autoload -Uz compinit
 
 if [[ ! -f $ZSH_COMPDUMP || -s $ZSH_COMPDUMP(#qN.mh+24) ]];then
-  # echo "Init and compil"
-  compinit -i -d $ZSH_COMPDUMP
+  compinit -d $ZSH_COMPDUMP
   zcompile $ZSH_COMPDUMP
 else
-  echo "Loaded from cache"
   compinit -C
 fi
 
