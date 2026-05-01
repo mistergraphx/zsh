cf0ee7f200b71bff732ffe7c6055564db2e410f4
mistergraphx <arnaud.berard@mister-graphx.com>
2026-05-01 10:48:50 +0200

    (functions): move functions in separate files (aliases): fix tiddlywiki alias to load node when starting

--- aliases.zsh
+++ aliases.zsh
@@ -12,8 +12,9 @@ alias emulator="$HOME/Library/Android/sdk/emulator/emulator"
 alias composer='/Applications/MAMP/bin/php/composer'
 
 # TIDDLY WIKI
-alias wiki-start="cd $HOME/Sites/TiddlyWiki && tiddlywiki GxWiki --listen"
+# launch node first
+alias wiki-start="cd $HOME/Sites/TiddlyWiki ; nvm_load ; node -v ; tiddlywiki GxWiki --listen"
 alias wiki-stop="kill -9 $(lsof -ti tcp:8080)" # Tiddly node server don't have any method to stop the process
-alias wiki-build="cd $HOME/Sites/TiddlyWiki && tiddlywiki GxWiki --rendertiddler $:/core/save/all index.html text/plain"
+alias wiki-build="cd $HOME/Sites/TiddlyWiki ; nvm_load ; node -v ; tiddlywiki GxWiki --rendertiddler $:/core/save/all index.html text/plain"
 
 # HELPERS
